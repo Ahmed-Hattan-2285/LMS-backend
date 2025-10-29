f90605256e6ba13f5f74a031fa7281a65b7a2097
Ahmed-Hattan85 <a.hattan.85@gmail.com>
2025-10-29 16:14:37 +0300

    Course model and Serialization

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Course
+from .serializers import CourseSerializer
 
-# Define the home view
 class Home(APIView):
   def get(self, request):
     content = {'message': 'Welcome to the learning management system api home route!'}
     return Response(content)
+  
+class CoursesIndex(APIView):
+  
+  serializer_class = CourseSerializer
+
+  def get(self, request):
+    try:
+      queryset = Course.objects.all()
+      serializer = self.serializer_class(queryset, many=True)
+      return Response(serializer.data, status=status.HTTP_200_OK)
+    except Exception as err:
+      return Response({'error': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
