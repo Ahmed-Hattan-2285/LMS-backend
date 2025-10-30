fd92b21c4c127e6058cc00145bdc3bac0f4eacca
Ahmed-Hattan85 <a.hattan.85@gmail.com>
2025-10-30 09:04:53 +0300

    update

--- main_app/views.py
+++ main_app/views.py
@@ -4,30 +4,44 @@ from rest_framework import status
 from .models import Course
 from .serializers import CourseSerializer
 
-class Home(APIView):
-  def get(self, request):
-    content = {'message': 'Welcome to the learning management system api home route!'}
-    return Response(content)
-  
-class CoursesIndex(APIView):
-  
-  serializer_class = CourseSerializer
 
-  def get(self, request):
-    try:
-      queryset = Course.objects.all()
-      serializer = self.serializer_class(queryset, many=True)
-      return Response(serializer.data, status=status.HTTP_200_OK)
-    except Exception as err:
-      return Response({'error': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+class Home(APIView):
+    def get(self, request):
+        content = {
+            'message': 'Welcome to the learning management system api home route!'}
+        return Response(content)
+
+
+class CoursesIndex(APIView):
+
+    serializer_class = CourseSerializer
+
+    def get(self, request):
+        try:
+            queryset = Course.objects.all()
+            serializer = self.serializer_class(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response({'error': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def post(self, request):
+        try:
+            serializer = self.serializer_class(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as err:
+            return Response({'error': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class CourseDetail(APIView):
-  serializer_class = CourseSerializer
+    serializer_class = CourseSerializer
 
-  def get(self, request, id):
-    try:
-      course = Course.objects.get(id=id)
-      serializer = self.serializer_class(course)
-      return Response(serializer.data, status=status.HTTP_200_OK)
-    except Exception as err:
-      return Response({'error': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def get(self, request, id):
+        try:
+            course = Course.objects.get(id=id)
+            serializer = self.serializer_class(course)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response({'error': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
